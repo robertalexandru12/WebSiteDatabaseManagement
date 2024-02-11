6cc6c62559ad7b3dcb60f73919f70174a5ada83d
Robert Biris <49343465+robertalexandru12@users.noreply.github.com>
2024-02-11 13:31:47 +0200

    Update LucrareDaoImpl.java
    modificari

--- src/main/java/DAOImpl/LucrareDaoImpl.java
+++ src/main/java/DAOImpl/LucrareDaoImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAOImpl;
 
 import java.util.ArrayList;
@@ -16,10 +11,6 @@ import java.util.Date;
 import pojo.Constructori;
 import pojo.Clienti;
 
-/**
- *
- * @author vali
- */
 public class LucrareDaoImpl implements LucrareDao{
 
     public void adaugaLucrare(Lucrare lucrare) {
